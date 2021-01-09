8057cf46aaa5644b7d61bc107f0a348cb61263cf
Sandy Perez Matamoros <sandypm007@gmail.com>
2021-01-09 07:48:48 -0500

    Fixed template logout button

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -50,19 +50,11 @@
                         <div class="dropdown-menu" aria-labelledby="dropdown-user">
                             <a class="dropdown-item" href="{{ route('chat') }}">Chat</a>
                             <a class="dropdown-item" href="{{ route('configuration') }}">Configuración</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                @csrf
-                                <button class="dropdown-item" type="submit">Salir</button>
-                            </form>
+                            <a class="dropdown-item" href="{{ route('logout') }}">Salir</a>
                         </div>
                     </li>
                 @else
-                    <li class="nav-item">
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                            @csrf
-                            <button class="nav-link" type="submit">Salir</button>
-                        </form>
-                    </li>
+                    <li class="nav-item"><a href="{{ route('logout') }}" class="nav-link">Salir</a></li>
                 @endif
             </ul>
         </div>
